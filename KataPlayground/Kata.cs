dca8d8609991a4d63b40dd2e6c1da08ea131b4d0
Nathan McGinn <nmcginn@users.noreply.github.com>
2020-11-14 14:24:13 -0600

    first kata

---
+++ KataPlayground/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace kata_playground
+{
+    public class Kata
+    {
+        public static string ToCamelCase(string str)
+        {
+            var words = str.Split(new char[] {'_', '-'});
+            return words.First() + string.Join("", words
+               .Skip(1)
+               .Select(word => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word))
+            );
+        }
+    }
+}
